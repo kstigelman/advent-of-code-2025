93b7528a723aeedea86a9fd9717d60312a5d000b
Kyler Stigelman <kystiges23@gmail.com>
2025-12-01 16:44:56 -0500

    That was overcomplicated

--- src/day1.rs
+++ src/day1.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-pub fn password (file_path: &str) -> u32 {
+pub fn password (file_path: &str) -> i32 {
 
     let mut current_position = 50;
     let mut count = 0;
@@ -11,6 +11,7 @@ pub fn password (file_path: &str) -> u32 {
     match file {
         Ok(f) => {
             let reader = BufReader::new (f);
+            let mut line_counter = 1;
             for line in reader.lines () {
                 let current_line = match line {
                     Ok(s) => s,
@@ -34,18 +35,64 @@ pub fn password (file_path: &str) -> u32 {
                 let trimmed_move = move_instruction[1].trim();
                 let num: i32 = trimmed_move.parse().expect("Failed to parse string to i32");
 
+                let mut tick = num;
+
+                while tick > 0 {
+                    current_position += direction;
+                    current_position = (100 + current_position) % 100;
+
+                    if current_position == 0 {
+                        count += 1;
+                    }
+                    
+                    tick -= 1;
+                }
+                /*let mut prev_position = current_position;
+                
                 current_position += num * direction;
 
-                while current_position < 0 {
-                    current_position += 100;
+
+                
+                let mut times_passed_zero = 0;
+                let end_position = current_position;
+
+                while current_position < 0 || current_position > 99 {
+                    current_position += 100 * -direction;
+                    times_passed_zero += 1;
                 }
-                while current_position > 99 {
-                    current_position -= 100;
+                
+
+                
+                
+                if num < 100 {
+                    count += times_passed_zero;
+                    if current_position == 0 {
+                        count += 1
+                    }
+                    
+                }
+                else if num % 100 == 0 {
+                    count += end_position.abs() / 100;
+                }
+                else {
+                    if direction < 0 {
+                        count += (end_position.abs() / 100)+ 1;
+                        
+                    }
+                    else {
+                        count += end_position.abs() / 100;
+                    }
                 }
 
-                if current_position == 0 {
-                    count += 1;
-                }
+
+                // case 1: move is less than 100
+              
+
+                           
+                */               
+                
+                //println!("Line {}: {}, {}", line_counter, current_position, count);
+                line_counter+=1;
 
             }
             return count;
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod day1;
 fn main() {
     // Day 1
     println!("Day 1 Solution: {}", day1::password("src/data/day1.txt"));
+    
 }
